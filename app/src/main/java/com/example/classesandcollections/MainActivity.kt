611373a4f439ff97947a6f5428ebac7ac4efadf9
Zifa <zifa04@mail.ru>
2025-11-11 16:32:31 +0300

    task1

--- app/src/main/java/com/example/classesandcollections/MainActivity.kt
+++ app/src/main/java/com/example/classesandcollections/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.classesandcollections
 
+// Task 1
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val dayPart: String,
+    val durationInMinutes: Int,
+)
+
 fun main(){
 
 }
