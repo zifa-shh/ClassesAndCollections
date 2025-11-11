1e790c3117d3e874061ca10f9cfe525d1be14530
Zifa <zifa04@mail.ru>
2025-11-11 16:33:06 +0300

    task3

--- app/src/main/java/com/example/classesandcollections/MainActivity.kt
+++ app/src/main/java/com/example/classesandcollections/MainActivity.kt
@@ -15,6 +15,18 @@ enum class DayPart {
     EVENING,
 }
 
+// Task 3
+fun task3(): List<Event> {
+    val event1 = Event(title = "Wake up", description = "Time to get up", dayPart = DayPart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", dayPart = DayPart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", dayPart = DayPart.AFTERNOON, durationInMinutes = 30)
+    val event4 = Event(title = "Practice Compose", dayPart = DayPart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", dayPart = DayPart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", dayPart = DayPart.EVENING, durationInMinutes = 45)
+    val events = mutableListOf(event1, event2, event3, event4, event5, event6)
+    return events
+}
+
 fun main(){
 
 }
